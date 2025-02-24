e0f82e693471a9f4be9f55092e83d17cfb4e3973
Greegko <greegko+github@gmail.com>
2025-02-24 14:54:39 +0100

    support local dev

--- packages/web/src/app.tsx
+++ packages/web/src/app.tsx
@@ -1,7 +1,26 @@
-import data from "./data.json";
+import { Show, createResource } from "solid-js";
+import { isDev } from "solid-js/web";
+
+import { CVData } from "./data";
 import { CvDisplayPage } from "./pages/cv-display";
 import { ErrorPage } from "./pages/error";
 
+async function getCvData(): Promise<CVData> {
+  if (isDev) {
+    const localData = await import("../../data/data.json");
+    return localData.default;
+  } else {
+    const response = await fetch("lambda-url");
+    return response.json();
+  }
+}
+
 export const App = () => {
-  return data ? <CvDisplayPage data={data} /> : <ErrorPage />;
+  const [data] = createResource<CVData>(() => getCvData());
+
+  return (
+    <Show when={data()} fallback={data.loading ? null : <ErrorPage />} keyed>
+      {data => <CvDisplayPage data={data} />}
+    </Show>
+  );
 };
